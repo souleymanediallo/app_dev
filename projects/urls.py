90b1149545fcd5473055e6de88be8b0599c5845a
souleymanediallo <souleymanedialloo@yahoo.fr>
2023-02-04 19:39:24 +0100

    add projects

--- projects/urls.py
+++ projects/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
+    path('projects', views.project_list, name="project-list"),
+    path('detail/<str:pk>', views.project_detail, name="project-detail"),
 ]
--- projects/views.py
+++ projects/views.py
@@ -1,6 +1,37 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+projectsList = [
+    {
+        'id': '1',
+        'title': 'Ecommerce Website',
+        'description': 'Fully functional ecommerce website'
+    },
+    {
+        'id': '2',
+        'title': 'Portfolio Website',
+        'description': 'A personal website to write articles and display work'
+    },
+    {
+        'id': '3',
+        'title': 'Social Network',
+        'description': 'An open source project built by the community'
+    }
+]
 
 
 # Create your views here.
 def home(request):
     return render(request, "projects/index.html")
+
+
+def project_list(request):
+    context = {'projects': projectsList}
+    return render(request, "projects/project_list.html", context)
+
+
+def project_detail(request, pk):
+    projectObj = None
+    for i in projectsList:
+        if i['id'] == pk:
+            projectObj = i
+    return render(request, "projects/project_detail.html", {"project": projectObj})
